27b62fb64600177d82f0328934d2eb05d064296f
INSEONG LEE <inseong101@naver.com>
2026-09-22 19:34:00 +0900

    Keep only run.py and Colab notebook alongside data and figures

--- run.py
+++ run.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
-"""Preprocess the two datasets and draw Figure 1."""
+"""Preprocess the two datasets and draw Figure 1.
+
+Local setup: python -m pip install "matplotlib>=3.9,<4" "tqdm>=4.66,<5"
+Run: python run.py
+Generated tables and counts are written to work/.
+"""
+
+# MIT License
+#
+# Copyright (c) 2026 inseong101
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import csv
 import hashlib
